bed4cc47b7c6f6c5e824709e64b55a9ad4732a4e
Gustav Lillesæter <gustav.lillesater@nav.no>
2022-01-06 10:10:03 +0100

    upgrade dependencies

--- src/decorator/decorator.ts
+++ src/decorator/decorator.ts
@@ -1,5 +1,22 @@
-import { setParams } from '@navikt/nav-dekoratoren-moduler'
-import { Params } from '@navikt/nav-dekoratoren-moduler/dist/functions/params'
+import { Locale, setParams } from '@navikt/nav-dekoratoren-moduler'
+
+export interface Params {
+  context?: "privatperson" | "arbeidsgiver" | "samarbeidspartner";
+  simple?: boolean;
+  enforceLogin?: boolean;
+  redirectToApp?: boolean;
+  redirectToUrl?: string;
+  level?: string;
+  language?: Locale;
+  utilsBackground?: "white" | "gray" | "transparent";
+  feedback?: boolean;
+  chatbot?: boolean;
+  taSurveys?: string;
+  urlLookupTable?: boolean;
+  shareScreen?: boolean;
+  utloggingsvarsel?: boolean;
+  logoutUrl?: string;
+}
 
 const DEFAULT_PARAMS: Params = {
   chatbot: false,
