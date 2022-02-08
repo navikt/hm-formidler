8a3e26bd078381d3e45b8751bf4e6caaafac9b3c
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2022-02-08 15:20:59 +0100

    DIGIHOT-700 vis info om elektrisk rullestol og personløfter

--- src/components/Hjelpemiddel.tsx
+++ src/components/Hjelpemiddel.tsx
@@ -4,6 +4,7 @@ import { HjelpemiddelItem } from '../interfaces/CommonTypes'
 import Hjelpemiddeltekstinfo from './Hjelpemiddeltekstinfo'
 import { RullestolInfo } from './RullestolInfo'
 import { Kroppsmaal } from '../interfaces/Brukerinfo'
+import { Kategori } from '../soknad/kategorier'
 
 type HjelpemiddelProps = {
   hm: HjelpemiddelItem
@@ -24,7 +25,10 @@ const Hjelpemiddel = (props: HjelpemiddelProps) => {
       <div style={{ marginTop: hm.vilkarliste || hm.tilleggsinformasjon || hm.begrunnelse ? '0.5rem' : '' }}>
         <Hjelpemiddeltekstinfo hm={hm} />
       </div>
-      {hm.hjelpemiddelkategori === 'Manuelle rullestoler' && <RullestolInfo hm={hm} kroppsmaal={kroppsmaal} />}
+      {(hm.hjelpemiddelkategori === Kategori.ManuelleRullestoler ||
+        hm.hjelpemiddelkategori === Kategori.ElektriskeRullestoler) && (
+        <RullestolInfo hm={hm} kroppsmaal={kroppsmaal} />
+      )}
     </>
   )
 }
---
+++ src/soknad/kategorier.ts
@@ -0,0 +1,17 @@
+export enum Kategori {
+  AlarmOgVarslingshjelpemidler = 'Alarm- og varslingshjelpemidler',
+  Arbeidsstoler = 'Arbeidsstoler',
+  Ganghjelpemidler = 'Ganghjelpemidler',
+  Hygienehjelpemidler = 'Hygienehjelpemidler',
+  HørselsOgVarslingshjelpemidler = 'Hørsels- og varslingshjelpemidler',
+  KalendereOgPlanleggingssystemer = 'Kalendere og planleggingssystemer',
+  KjøreposerVarmeposerOgRegncape = 'Kjøreposer, varmeposer og regncape',
+  Madrasser = 'Madrasser',
+  ManuelleRullestoler = 'Manuelle rullestoler',
+  PersonløftereOgSeil = 'Personløftere og seil',
+  SengerOgSengebunner = 'Senger, sengebunner m.m.',
+  Sitteputer = 'Sitteputer',
+  SkliVendeOgFlytte = 'Skli, vende og flytte',
+  TerskeleliminatorerOgRamper = 'Terskeleliminatorer og ramper',
+  ElektriskeRullestoler = 'Elektriske rullestoler',
+}
