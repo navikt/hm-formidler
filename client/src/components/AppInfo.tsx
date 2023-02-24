b1131dea8252bdd07498f2156481176ef6e58425
Ole Steinar Lillestøl Skrede <3756410+olesls@users.noreply.github.com>
2023-02-24 12:48:20 +0100

    Nye felter for kalenderapp (#60)
    * Nye felter kalenderapp
    
    * fix import
    
    * fiks null-sjekk og legg til testdata
    
    * Conditional render i stedet for return null
    
    * formating

---
+++ client/src/components/AppInfo.tsx
@@ -0,0 +1,46 @@
+import { BodyShort, Label } from '@navikt/ds-react'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { HjelpemiddelItem } from '../interfaces/CommonTypes'
+
+type Props = {
+  hm: HjelpemiddelItem
+}
+
+export const AppInfo: React.FC<Props> = (props: Props) => {
+  const { t } = useTranslation()
+  const { hm } = props
+
+  return (
+    <>
+      <div style={{ marginBottom: '0.5rem' }}>
+        <Label style={{ display: 'inline' }}>{t('hjelpemiddelinfo.kalenderapp.utprøvingForBruker.label')} </Label>
+        <BodyShort style={{ display: 'inline' }}>
+          {hm.appInfo?.brukerHarProvdProvelisens
+            ? t('hjelpemiddelinfo.kalenderapp.utprøvingForBruker.harPrøvd')
+            : t('hjelpemiddelinfo.kalenderapp.utprøvingForBruker.harIkkePrøvd')}
+        </BodyShort>
+      </div>
+
+      <div style={{ marginBottom: '0.5rem' }}>
+        <Label style={{ display: 'inline' }}>{t('hjelpemiddelinfo.kalenderapp.støtteperson.label')} </Label>
+        <BodyShort style={{ display: 'inline' }}>
+          {hm.appInfo?.stottepersonSkalAdministrere
+            ? t('hjelpemiddelinfo.kalenderapp.støtteperson.skalAdministrere')
+            : t('hjelpemiddelinfo.kalenderapp.støtteperson.skalIkkeAdministrere')}
+        </BodyShort>
+      </div>
+
+      {hm.appInfo?.stottepersonSkalAdministrere && hm.appInfo.stottepersonHarProvdProvelisens != null && (
+        <div style={{ marginBottom: '0.5rem' }}>
+          <Label style={{ display: 'inline' }}>{t('hjelpemiddelinfo.kalenderapp.utprøvingForStøtteperson.label')} </Label>
+          <BodyShort style={{ display: 'inline' }}>
+            {hm.appInfo.stottepersonHarProvdProvelisens
+              ? t('hjelpemiddelinfo.kalenderapp.utprøvingForStøtteperson.harPrøvd')
+              : t('hjelpemiddelinfo.kalenderapp.utprøvingForStøtteperson.harIkkePrøvd')}
+          </BodyShort>
+        </div>
+      )}
+    </>
+  )
+}
