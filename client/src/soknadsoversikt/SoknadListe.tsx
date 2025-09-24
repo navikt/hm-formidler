b8e4bee215e214f8c36e7614e9b8162fbccf8def
Zandra7 <sandra.eng@live.no>
2025-09-24 08:49:23 +0200

    Flytter 'Ny sak' knapp under GuidePanel

--- client/src/soknadsoversikt/SoknadListe.tsx
+++ client/src/soknadsoversikt/SoknadListe.tsx
@@ -3,7 +3,9 @@ import './../stylesheet/styles.scss'
 import { SoknadInfo } from '../interfaces/SoknadInfo'
 import SoknadKort from './SoknadKort'
 import { useTranslation } from 'react-i18next'
-import { Heading } from '@navikt/ds-react'
+import { Button, Heading } from '@navikt/ds-react'
+import environment from '../environment'
+import { digihot_customevents, logCustomEvent } from '../utils/amplitude'
 
 interface Props {
   alleSoknader: SoknadInfo[]
@@ -14,8 +16,20 @@ const SoknadListe: React.FC<Props> = ({ alleSoknader }: Props) => {
 
   return (
     <div className="customPanel">
-      <div style={{ marginBottom: '1rem' }}>
-        <Heading level="2" size="small">{t('soknadsoversikt.soknadListe.saker')}</Heading>
+      <div className="sakerNySakWrapper">
+        <Heading level="2" size="small">
+          {t('soknadsoversikt.soknadListe.saker')}
+        </Heading>
+        <a href={environment.SOKNAD_URL}>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              logCustomEvent(digihot_customevents.KLIKK_NY_SAK, { steg: -1 })
+            }}
+          >
+            Ny sak
+          </Button>
+        </a>
       </div>
       {alleSoknader.map((soknad: SoknadInfo) => {
         return (
