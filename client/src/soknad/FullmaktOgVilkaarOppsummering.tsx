211c89750bc6a80b74617a0921d432deecf753d6
murvold <mads.severin.murvold@nav.no>
2025-10-03 15:47:35 +0200

    Gjør om signaturkomponent til å bruke FormSummary

--- client/src/soknad/FullmaktOgVilkaarOppsummering.tsx
+++ client/src/soknad/FullmaktOgVilkaarOppsummering.tsx
@@ -1,67 +1,94 @@
 import React from 'react'
-import { Label, BodyShort, Box } from '@navikt/ds-react'
-
+import { Label, BodyShort, FormSummary } from '@navikt/ds-react'
 import { useTranslation } from 'react-i18next'
-import { Bruker, Brukersituasjon, Innsender, Innsenderrolle, Signaturtype } from '../interfaces/Innsenderbehovsmelding'
+import { Bruker, Signaturtype } from '../interfaces/Innsenderbehovsmelding'
 import { formaterPersonnavn } from '../interfaces/CommonTypes'
-import { lokaliser } from './OpplysningVisning'
+import { Avstand } from '../components/Avstand'
 
 type FullmaktOgVilkaarProps = {
   bruker: Bruker
-  brukersituasjon: Brukersituasjon
-  innsender: Innsender
 }
 
 const FullmaktOgVilkaarOppsummering: React.FC<FullmaktOgVilkaarProps> = (props: FullmaktOgVilkaarProps) => {
   const { t } = useTranslation()
 
-  const { bruker, brukersituasjon, innsender } = props
+  const { bruker } = props
 
   const brukersNavn = formaterPersonnavn(bruker.navn)
 
   return (
     <>
-      <hr aria-hidden="true" />
-
       {bruker.signaturtype === Signaturtype.FRITAK_FRA_FULLMAKT && (
-        <div className="contentBlock">
-          <Label>{t('oppsummering.fritakFraFullmakt.tittel')}</Label>
-          <ul>
-            <li>
-              <BodyShort>{t('oppsummering.fritakFraFullmakt.tekst')}</BodyShort>
-            </li>
-            <li>
-              <BodyShort>{t('oppsummering.brukerErInformertOmRettigheter', { navn: brukersNavn })}</BodyShort>
-            </li>
-          </ul>
-        </div>
+        <FormSummary>
+          <FormSummary.Header><FormSummary.Heading level="2">{t('oppsummering.fritakFraFullmakt.tittel')}</FormSummary.Heading></FormSummary.Header>
+          <FormSummary.Answers>
+            <FormSummary.Answer>
+              <FormSummary.Label className='screenreaderOnly'>{t('oppsummering.label')}</FormSummary.Label>
+              <FormSummary.Value>
+                <ul>
+                  <li>
+                    <BodyShort>{t('oppsummering.fritakFraFullmakt.tekst')}</BodyShort>
+                  </li>
+                  <li>
+                    <BodyShort>{t('oppsummering.brukerErInformertOmRettigheter', { navn: brukersNavn })}</BodyShort>
+                  </li>
+                </ul>
+              </FormSummary.Value>
+            </FormSummary.Answer>
+          </FormSummary.Answers>
+        </FormSummary>
       )}
 
-      <Box.New maxWidth="600px" className="contentBlock">
-        <div className="contentBlock">
-          <Label>
-            {innsender.rolle === Innsenderrolle.FORMIDLER
-              ? t('oppsummering.formidlerVurdert')
-              : t('oppsummering.bestillerVurdert')}
-          </Label>
-
-          <ul
-            style={{
-              fontFamily: '"Source Sans Pro",Arial,sans-serif',
-              lineHeight: '1.375rem',
-            }}
-          >
-            {brukersituasjon.vilkår.map((vilkår, i) => {
-              return <li key={i}>{lokaliser(vilkår.tekst)}</li>
-            })}
-          </ul>
-        </div>
-
-        <div className="contentBlock">
-          <Label spacing>{t('oppsummering.infoOmRettOgPlikt.tittel')}</Label>
-          <BodyShort>{t('oppsummering.infoOmRettOgPlikt')}</BodyShort>
-        </div>
-      </Box.New>
+      {bruker.signaturtype !== Signaturtype.BRUKER_BEKREFTER && (
+        <FormSummary>
+          <FormSummary.Header><FormSummary.Heading level="2">{t("oppsummering.signatur")}</FormSummary.Heading></FormSummary.Header>
+          <FormSummary.Answers>
+            {bruker.signaturtype === Signaturtype.FULLMAKT && (
+              <FormSummary.Answer>
+                <FormSummary.Label className="screenreaderOnly">{t("oppsummering.label")}</FormSummary.Label>
+                <FormSummary.Value>
+                  <ul>
+                    <li>
+                      <BodyShort>
+                        {t('oppsummering.signertFullmakt', {
+                          navn: brukersNavn,
+                        })}
+                      </BodyShort>
+                    </li>
+                    <li>
+                      <BodyShort>{t('oppsummering.arkivertFullmakt')}</BodyShort>
+                    </li>
+                  </ul>
+                </FormSummary.Value>
+              </FormSummary.Answer>
+            )}
+            {bruker.signaturtype === Signaturtype.IKKE_INNHENTET_FORDI_BYTTE && (
+              <FormSummary.Answer>
+                <FormSummary.Label>{t('oppsummering.ikkeFullmaktFordiBytte.tittel')}</FormSummary.Label>
+                <FormSummary.Value>{t('oppsummering.ikkeFullmaktFordiBytte.tekst')}</FormSummary.Value>
+              </FormSummary.Answer>
+            )}
+            {bruker.signaturtype === Signaturtype.IKKE_INNHENTET_FORDI_KUN_TILBEHØR_V3 && (
+              <FormSummary.Answer>
+                <FormSummary.Label>{t('bruker.fullmakt.tittel')}</FormSummary.Label>
+                <FormSummary.Value>{t('oppsummering.ikkeFullmaktFordiTilbehør.tekst')}</FormSummary.Value>
+                <FormSummary.Value>
+                  <ul>
+                    <li>
+                      {t('oppsummering.ikkeFullmaktFordiTilbehør.innbyggerBekrefter')}
+                    </li>
+                  </ul>
+                </FormSummary.Value>
+              </FormSummary.Answer>
+            )}
+          </FormSummary.Answers>
+        </FormSummary>
+      )}
+      <div className="contentBlock">
+        <Avstand marginTop={10} />
+        <Label spacing>{t('oppsummering.infoOmRettOgPlikt.tittel')}</Label>
+        <BodyShort>{t('oppsummering.infoOmRettOgPlikt')}</BodyShort>
+      </div>
     </>
   )
 }
