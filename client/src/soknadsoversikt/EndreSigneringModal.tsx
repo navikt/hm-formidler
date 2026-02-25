29ab385cc1935a688dd21522d4e4dd9cb9395b04
Ole Steinar L. Skrede <ole.steinar.lillestol.skrede@nav.no>
2026-02-25 08:46:26 +0100

    Debug med POST i stedet for PUT

--- client/src/soknadsoversikt/EndreSigneringModal.tsx
+++ client/src/soknadsoversikt/EndreSigneringModal.tsx
@@ -33,7 +33,7 @@ export const EndreSigneringModal = (props: EndreSigneringModalProps) => {
     setError(null)
     try {
       const response = await fetch(`${API_PATH}/behovsmelding/${soknadsid}/brukerbekreftelse-til-fullmakt`, {
-        method: 'PATCH',
+        method: 'POST',
         credentials: 'same-origin',
       })
 
