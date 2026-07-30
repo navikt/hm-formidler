99342ed7e123169742513e2c9fcb88723c3eade4
Zandra7 <sandra.eng@live.no>
2026-07-30 09:46:25 +0200

    Legger til de nye feltene

--- client/src/soknad/OpplæringsAnsvarligOppsummering.tsx
+++ client/src/soknad/OpplæringsAnsvarligOppsummering.tsx
@@ -47,6 +47,18 @@ const OpplæringsAnsvarlig: React.FC<OpplæringsAnsvarligProps> = (props: Opplæ
                 <FormSummary.Label>{t('oppsummering.ansvar')}</FormSummary.Label>
                 <FormSummary.Value>{oppfolgingsansvarlig.ansvarFor}</FormSummary.Value>
               </FormSummary.Answer>
+              {oppfolgingsansvarlig.epost && (
+                <FormSummary.Answer>
+                  <FormSummary.Label>{t('felles.epost')}</FormSummary.Label>
+                  <FormSummary.Value>{oppfolgingsansvarlig.epost}</FormSummary.Value>
+                </FormSummary.Answer>
+              )}
+              {oppfolgingsansvarlig.erGjortOppmerksomPåOpplæringsansvar && (
+                <FormSummary.Answer>
+                  <FormSummary.Label>{t('oppsummering.oppmerksom')}</FormSummary.Label>
+                  <FormSummary.Value>Ja</FormSummary.Value>
+                </FormSummary.Answer>
+              )}
             </>
           )}
         </FormSummary.Answers>
